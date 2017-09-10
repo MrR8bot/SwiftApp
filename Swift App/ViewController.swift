d3bc5b94ccb5d20bd55b66f2aa89ef349b061e5a
MrR8bot <carlos.ave@gmail.com>
2017-09-10 16:13:28 +1000

    Swift Test

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+      @IBOutlet weak var TheLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func PushMe(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            TheLabel.text = " You tapped the butotn 10 times"
+        }
+        
+      
+        
+    }
+  
+    @IBAction func ButtonPushed(_ sender: Any) {
+        
+        TheLabel.text = "Buttons are cool"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
